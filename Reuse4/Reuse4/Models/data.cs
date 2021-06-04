10231158fd41181b302d88c99140fe7054d979a1
Saikiran Challa <saikiran7399@gmail.com>
2021-06-04 14:09:32 +0530

    Hello

---
+++ Reuse4/Reuse4/Models/data.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Reuse4.Models
+{
+    public class data
+    {
+        public int Empid
+        {
+            get;
+            set;
+        }
+        public string Empname
+        {
+            get;
+            set;
+        }
+    }
+}
